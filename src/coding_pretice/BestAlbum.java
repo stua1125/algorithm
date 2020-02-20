e4044977bf86be877e79bc01ef317e58d6551efc
one <one@DESKTOP-LTI1IM5>
2020-02-21 00:22:18 +0900

    베스트앨범 bestAlbum init

--- src/coding_pretice/BestAlbum.java
+++ src/coding_pretice/BestAlbum.java
@@ -1,7 +1,7 @@
 package coding_pretice;
 
+import java.io.ObjectOutputStream.PutField;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,77 +17,114 @@ public class BestAlbum {
 		int[] plays = {500, 600, 150, 800, 2500};
 		
 		
-		Map<String, Integer> countMap = new HashMap<>();
+		Map<String, Integer> totalCount = new HashMap<>();
+		Map<Integer, String> gMap = new HashMap<>();
+		Map<Integer, Integer> pMap = new HashMap<>();
 		for(int i = 0; i < genres.length; i++) {
-			countMap.put(genres[i], plays[i]);
+			int a = totalCount.get(genres[i]) == null ? 0 : totalCount.get(genres[i]); // 삼항연산자
+			totalCount.put(genres[i], plays[i] + a);
+			
+			gMap.put(i, genres[i]); // i is index 
+			pMap.put(i, plays[i]);
 		}
 		
-		
-		
-		
-		
-		int n = 7;
-		Integer[] input = {7, 10, 1, 2, 7, 7, 1};
-		
-		List<Integer> listA = Arrays.asList(input);
-		
-//		listA.size()
-		
-//		Map<Integer, Integer> countMap = new HashMap<>();
-		
-		// listA �迭�� �ؽ��� Ÿ������ ���ڸ� key�� �ΰ� value�� count�� ���
-		listA.forEach(e -> {
-			Integer count = countMap.get(e);
-			countMap.put(e, count == null ? 1 : count + 1);
-		});
-		
-		// �ʿ��� ������ �ϱ����� list<list<int>>������ ��ȯ
-		List<List<Integer>> resultList = new ArrayList<>();
-		countMap.forEach( (k, v) ->{
-			System.out.println("k" + k);
-			System.out.println("v" + v);
-			List<Integer> list = new ArrayList<>();
-			for(int i = 0; i < v; i++) {
-				list.add(k);
+		for(String item : totalCount.keySet()) {		
+			System.out.print("genres Name : " + item + ",  total Sum : ");
+			System.out.println(totalCount.get(item));
+
+			// 해당 장르의 인덱스 배열 임시 저장
+			List sameGenresIndexArray = new ArrayList<>();
+			
+			
+			// 높은 순서대로 포문 돌림
+			for(Integer tt : gMap.keySet()) {
+//				System.out.print(tt + " : ");
+//				System.out.println(gMap.get(tt));
+//				System.out.println("================================");
+				if(item.equals(gMap.get(tt))) {
+					sameGenresIndexArray.add(tt);
+				}
 			}
-			resultList.add(list);
-		});
-		
-		System.out.println(resultList);
-		// list ���� ���� ������� ���� (���ٷ� �ؽ� Ű�� ������ ������)
-		resultList.sort( (a,b) -> {
-			if(a.size() > b.size()) {
-				return -1;
-			} else if(a.size() == b.size()){
-				return 0;
-			} else {
-				return 1;
+			
+			Map<Integer, Integer> rankMap = new HashMap<>();
+			List sameGenresPlayRank = new ArrayList<>();
+			
+			sameGenresIndexArray.forEach(e -> {
+//				System.out.print("sameGenresIndexArray : ");
+//				System.out.println(e);
+				sameGenresPlayRank.add(e);
+				rankMap.put((Integer) e, plays[(int) e]);
+			});
+			
+			
+			// 높은 순서대로 포문 돌림
+			for(Integer xx : rankMap.keySet()) {
+				System.out.print("index : " + xx + ",     value : ");
+				System.out.println(rankMap.get(xx));
+				System.out.println("================================");
 			}
-		}); 
-		
-		resultList.forEach( (a) -> {
-			System.out.println("a : " + a);
-//			System.out.println("count : " + count);
-		});
-		
-		
-		int is_half = 0;
-		int count = 0;
-		
-		// �迭 ����� �� ���ϰ� �Ǿ����� üũ
-		for(int i = 0; i < resultList.size(); i++) {
-			is_half += resultList.get(i).size();
-			count++;
-			if(is_half > n/2) {
-				break;
-			}
+			
+			
+			
+//			for(Integer ss : pMap.keySet()) {
+//				System.out.print(ss + " : ");
+//				System.out.println(pMap.get(ss));
+//				System.out.println("================================");
+//			}
+			System.out.println("##########################");
+			System.out.println("#                        #");
+			System.out.println("##########################");
 		}
-		
-		System.out.println("is_half : " + is_half);
-		System.out.println("count : " + count);
-		
-		
-		System.out.println(resultList);
+
+		//		listA.forEach(e -> {
+//			Integer count = countMap.get(e);
+//			countMap.put(e, count == null ? 1 : count + 1);
+//		});
+//		
+//		List<List<Integer>> resultList = new ArrayList<>();
+//		countMap.forEach( (k, v) ->{
+//			System.out.println("k" + k);
+//			System.out.println("v" + v);
+//			List<Integer> list = new ArrayList<>();
+//			for(int i = 0; i < v; i++) {
+//				list.add(k);
+//			}
+//			resultList.add(list);
+//		});
+//		
+//		System.out.println(resultList);
+//		resultList.sort( (a,b) -> {
+//			if(a.size() > b.size()) {
+//				return -1;
+//			} else if(a.size() == b.size()){
+//				return 0;
+//			} else {
+//				return 1;
+//			}
+//		}); 
+//		
+//		resultList.forEach( (a) -> {
+//			System.out.println("a : " + a);
+////			System.out.println("count : " + count);
+//		});
+//		
+//		
+//		int is_half = 0;
+//		int count = 0;
+//		
+//		for(int i = 0; i < resultList.size(); i++) {
+//			is_half += resultList.get(i).size();
+//			count++;
+//			if(is_half > n/2) {
+//				break;
+//			}
+//		}
+//		
+//		System.out.println("is_half : " + is_half);
+//		System.out.println("count : " + count);
+//		
+//		
+//		System.out.println(resultList);
 	}
 
 }
