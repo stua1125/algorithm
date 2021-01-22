5e2e57a80410f243ecbe3166786b7da6b64cdac8
stua92 <stau92@gmail.com>
2021-01-22 23:39:53 +0900

    leetcode 27 removeElement first init

---
+++ src/leetCode/RemoveElement.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class RemoveElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public int removeElement(int[] nums, int val) {
+		return 0;
+    }
+
+}
